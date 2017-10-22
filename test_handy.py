8bdd6a66a5d2b382221232b13d520d8a6e62239d
humkeym <mhhplumber@users.noreply.github.com>
2017-10-22 04:04:13 -0700

    minor functionality fix in test case

--- test_handy.py
+++ test_handy.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 
 
 def main():
-    print(urlopen("127.0.0.1:5000/create_new_task", data=jsonify({'name':'Haylee'})))
+    print(urlopen("127.0.0.1:5000/create_new_task", data=jsonify(name='haylee')))
           
 
 main();
